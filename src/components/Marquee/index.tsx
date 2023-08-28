64b9c84a39157a9ecd2c91f52fcf7d5047655a50
Santiago Jose Vitelli <gonsanto07@yahoo.com>
2023-08-28 12:49:07 -0300

    update marquee component

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -3,10 +3,10 @@ import { StyledMarquee } from "./style";
 export const Marquee = () => {
   return (
     <StyledMarquee>
-      <p>
-        A man can&apos;t have enough basement swag - A man can&apos;t have
-        enough basement swag A man can&apos;A man can&apos;t have enough swag
-      </p>
+      <p>A man can&apos;t have enough basement swag</p>
+      <p>A man can&apos;t have enough basement swag</p>
+      <p>A man can&apos;t have enough basement swag</p>
+      <p>A man can&apos;t have enough basement swag</p>
     </StyledMarquee>
   );
 };
--- src/components/Marquee/style.ts
+++ src/components/Marquee/style.ts
@@ -1,10 +1,32 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const moveRtl = keyframes`
+0%{
+  transform: translateX(0);
+}
+
+100%{
+  transform: translateX(-100%);
+}
+
+`;
 
 export const StyledMarquee = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  overflow: hidden;
   width: 100%;
+  gap: 1rem;
+  border: 1px solid white;
+  padding: 1rem 0;
+  border-left: none;
+  border-right: none;
+  z-index: 0;
 
   p {
     font-size: 3rem;
     z-index: 0;
+    white-space: nowrap;
+    animation: ${moveRtl} 6000ms linear infinite;
   }
 `;
